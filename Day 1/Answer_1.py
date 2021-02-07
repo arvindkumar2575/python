6ca1a51944f6be60abcf8c933f4ff97aed0a159d
arvindkumar2575 <arvindkumar2575@gmail.com>
2021-02-07 14:58:15 +0530

    DAY 1 ASSIGNMENT

---
+++ Day 1/Answer_1.py
@@ -0,0 +1,7 @@
+x = int(input("Enter the X : "))
+y = int(input("Enter the Y : "))
+
+print("Addition of X and Y : ", x+y)
+print("Subtraction of X and Y : ", x-y)
+print("Multiplication of X and Y : ", x*y)
+print("Division of X and Y : ", x/y)
---
+++ Day 1/Answer_2.py
@@ -0,0 +1,4 @@
+x = int(input("Enter the X : "))
+y = int(input("Enter the Y : "))
+
+print("Result of X^Y(X to the power of Y) : ", x**y)
